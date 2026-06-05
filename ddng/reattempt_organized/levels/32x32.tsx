66dfab51714f24a0cb2f5a457601e58c74ff9f4a
Ronaldo De Leon II <redeleon2@up.edu.ph>
2026-06-05 21:48:30 +0800

    ugh

--- ddng/reattempt_organized/levels/32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="32x32" tilewidth="32" tileheight="32" tilecount="299" columns="23">
- <image source="../assets/sprite/kenney_tower-defense-top-down/Tilesheet/32x32.png" width="736" height="416"/>
-</tileset>
